0c458d0b200111309168797932efe1b93dd25b3a
Zachary3234 <86643087+Zachary3234@users.noreply.github.com>
2022-03-20 17:38:34 +0800

    Update

--- js/data.js
+++ js/data.js
@@ -1,100 +1,72 @@
 const data = new (function DataCollect() {
-    const svoGains = [
-        // self
-        [85,85,85,85,85,85,85,85,85],
-        [85,87,89,91,93,94,96,98,100],
-        [50,54,59,63,68,72,76,81,85],
-        [50,54,59,63,68,72,76,81,85],
-        [100,94,88,81,75,69,63,56,50],
-        [100,98,96,94,93,91,89,87,85],
-        // other
-        [85,76,68,59,50,41,33,24,15],
-        [15,19,24,28,33,37,41,46,50],
-        [100,98,96,94,93,91,89,87,85],
-        [100,89,79,68,58,47,36,26,15],
-        [50,56,63,69,75,81,88,94,100],
-        [50,54,59,63,68,72,76,81,85]
-    ];
-    var svoSelect = [4,4,4,4,4,4];
+    var dataObj = {ID: ''};
     
-    for (let i = 0; i < 6; i++) {
-        $('#svo-row-'+i)[0].innerHTML = '';
-        for (let j = 0; j < 9; j++) {
-            if (j==svoSelect[i]){
-                $('#svo-row-'+i)[0].innerHTML += '<div class="selected"><div>'+svoGains[i][j]+'</div><div>'+svoGains[6+i][j]+'</div></div>';
-            }else{
-                $('#svo-row-'+i)[0].innerHTML += '<div><div>'+svoGains[i][j]+'</div><div>'+svoGains[6+i][j]+'</div></div>';
+    return {
+        svoSelect: [4, 4, 4, 4, 4, 4],
+        svoGains: [
+            // self
+            [85, 85, 85, 85, 85, 85, 85, 85, 85],
+            [85, 87, 89, 91, 93, 94, 96, 98, 100],
+            [50, 54, 59, 63, 68, 72, 76, 81, 85],
+            [50, 54, 59, 63, 68, 72, 76, 81, 85],
+            [100, 94, 88, 81, 75, 69, 63, 56, 50],
+            [100, 98, 96, 94, 93, 91, 89, 87, 85],
+            // other
+            [85, 76, 68, 59, 50, 41, 33, 24, 15],
+            [15, 19, 24, 28, 33, 37, 41, 46, 50],
+            [100, 98, 96, 94, 93, 91, 89, 87, 85],
+            [100, 89, 79, 68, 58, 47, 36, 26, 15],
+            [50, 56, 63, 69, 75, 81, 88, 94, 100],
+            [50, 54, 59, 63, 68, 72, 76, 81, 85]
+        ],
+        setID: function (id) {
+            dataObj.ID = id;
+            debug && console.log('Set ID:', dataObj.ID);
+        },
+        getData: function () {
+            dataObj["SVO"] = calcSVO(this.svoGains, this.svoSelect).toFixed(2);
+            let selfGain;
+            let otherGain;
+            for (let i = 0; i < this.svoSelect.length; i++) {
+                selfGain = this.svoGains[i][this.svoSelect[i]];
+                otherGain = this.svoGains[6+i][this.svoSelect[i]];
+                dataObj["svo-"+(i+1)+"-self"] = selfGain;
+                dataObj["svo-"+(i+1)+"-other"] = otherGain;
             }
-        }
-        $('#svo-row-'+i).children().click(function (){
-            $('#svo-row-'+i).children().removeClass('selected');
-            $(this).addClass('selected');
-            for (let j = 0; j < 9; j++) {
-                if($($('#svo-row-'+i).children()[j]).is($(this))){
-                    svoSelect[i] = j;
+            return dataObj;
+        },
+        getCSV: function (dataArr) {
+            let text = '';
+            let keys = ["ID","SVO"];
+            for (let i = 0; i < this.svoSelect.length; i++) {
+                keys.push("svo-"+(i+1)+"-self");
+                keys.push("svo-"+(i+1)+"-other");
+            }
+            for (let i = 0; i < keys.length; i++) {
+                const key = keys[i];
+                text += key;
+                if (i < keys.length-1)
+                    text += ',';
+                else
+                    text += '\r\n';
+            }
+            for (let j = 0; j < dataArr.length; j++) {
+                for (let i = 0; i < keys.length; i++) {
+                    const key = keys[i];
+                    text += dataArr[j][key];
+                    if (i < keys.length-1)
+                        text += ',';
+                    else
+                        text += '\r\n';
                 }
             }
-        });
-    }
-    
-    var dataCollect = {
-        id : undefined,
-        SVO: undefined,
-        score : 0,
-    }
-
-    this.setID = function (id){
-        dataCollect.id = id;
-    }
-    this.setSVO = function (){
-        dataCollect.SVO = svoSelect[0];////////////
-    }
-    this.setScore = function (val){
-        dataCollect.score = val;
-    }
-    this.setData = function (prop,val){
-        dataCollect[prop] = val;
-    }
-    this.sendData = function (){
-        //ajax发送到服务器
-        $.ajax({
-            url: "https://server.yunapps.site:10000/", //请求的URL
-            timeout: 5000, //超时时间设置，单位毫秒
-            type: 'post', //请求方式，get或post
-            data: JSON.stringify(dataCollect), //请求所传参数，json格式
-        })
-        .fail(function (XMLHttpRequest, status){
-            console.log(status, XMLHttpRequest)
-            downloadCSV(JSON.stringify(dataCollect),'data','.json');
-        })
-        .done(function (XMLHttpRequest, status){
-            console.log(status, XMLHttpRequest)
-        });
-        // .always(function (XMLHttpRequest, status){
-        //     console.log(status, XMLHttpRequest);
-        // });
-
-    }
-    
-
-
-    function genCSVText(){
-        var csvText = '';
-
-        csvText += dataCollect['id']+',';
-        csvText += dataCollect['SVO']+',';
-        csvText += dataCollect['score']+',';
-
-        return csvText;
+            let uri = "data:text/csv;charset=utf-8,\ufeff" + encodeURIComponent(text);
+            let link = document.createElement('a');
+            link.href = uri;
+            link.download = "data.csv";
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        }
     }
 })();
-
-function downloadCSV(csvText,title="data",type=".csv"){
-    let uri = "data:text/csv;charset=utf-8,\ufeff" + encodeURIComponent(csvText);
-    var link = document.createElement('a');
-    link.href = uri;
-    link.download =  title+type;
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-}
